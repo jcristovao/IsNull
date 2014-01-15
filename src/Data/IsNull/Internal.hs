f651992395dc32c23a87fcb871dc8feee6cc2632
João Cristóvão <jmacristovao@gmail.com>
2014-01-15 14:20:45 +0000

    Problems with type families

--- src/Data/IsNull/Internal.hs
+++ src/Data/IsNull/Internal.hs
@@ -3,16 +3,19 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE IncoherentInstances  #-}
 {-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE DefaultSignatures    #-}
+{-# LANGUAGE FlexibleContexts     #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Data.IsNull.Internal where
 
-import qualified Data.MonoTraversable as O
 import qualified Data.Foldable        as F
+import qualified Data.Text            as T
+import qualified Data.Text.Lazy       as LT
+import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Set as Set
 import Control.Monad (liftM)
-import Data.Set as Set
-
-instance O.MonoFoldable (Either a b)
 
 class IsNull a where
   type Nullable a
@@ -23,24 +26,24 @@ class IsNull a where
   isN = isNull
 
   -- | the logical negation of @'isNull'@
-  notNull :: Nullable a -> Bool
-  notNull = not . isNull
+  {-notNull :: Nullable a -> Bool-}
+  {-notNull = not . isNull-}
 
   -- | Nested isNull
-  isNullN :: F.Foldable f => f (Nullable a) -> Bool
-  isNullN = F.all isNull
+  {-isNullN :: F.Foldable f => f (Nullable a) -> Bool-}
+  {-isNullN = F.all isNull-}
 
   -- | Nested isNotNull
-  notNullN :: F.Foldable f => f (Nullable a) -> Bool
-  notNullN = not . isNullN
+  {-notNullN :: F.Foldable f => f (Nullable a) -> Bool-}
+  {-notNullN = not . isNullN-}
 
   -- | Monadic Null
-  isNullM :: Monad m => m (Nullable a) -> m Bool
-  isNullM = liftM isNull
+  {-isNullM :: Monad m => m (Nullable a) -> m Bool-}
+  {-isNullM = liftM isNull-}
 
   -- | Monadic Nested Null
-  isNullNM :: (Monad m, F.Foldable f) => m (f (Nullable a)) -> m Bool
-  isNullNM = liftM isNullN
+  {-isNullNM :: (Monad m, F.Foldable f) => m (f (Nullable a)) -> m Bool-}
+  {-isNullNM = liftM isNullN-}
 
 
 instance IsNull Bool where
@@ -51,7 +54,7 @@ instance IsNull (Set.Set a) where
   type Nullable (Set.Set a) = Set.Set a
   isNull = Set.null
 
-instance (O.MonoFoldable f) => IsNull f where
-  type Nullable f = f
-  isNull = O.onull
+{-instance (O.MonoFoldable f) => IsNull f where-}
+  {-type Nullable f = f-}
+  {-isNull = O.onull-}
 
