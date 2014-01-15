c61b9fb3a5d7d78d455bf95bb95298fb98057d35
João Cristóvão <jmacristovao@gmail.com>
2014-01-15 09:59:02 +0000

    First experiences with type families.

--- src/Data/IsNull/Internal.hs
+++ src/Data/IsNull/Internal.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE IncoherentInstances  #-}
+{-# LANGUAGE TypeFamilies         #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Data.IsNull.Internal where
 
-import qualified Data.Foldable as F
+import qualified Data.MonoTraversable as O
+import qualified Data.Foldable        as F
+import Data.Set as Set
+
+type family Nullable ent
+type instance Nullable (Set.Set a) = a
+type instance Nullable Bool = Bool
 
 class IsNull a where
   isNull :: a -> Bool
@@ -19,8 +27,17 @@ class IsNull a where
   notNull :: a -> Bool
   notNull = not . isNull
 
-instance (F.Foldable f) => IsNull (f a) where
-  isNull = F.foldr (\_ _ -> False) True
+instance (O.MonoFoldable f) => IsNull f where
+  isNull = O.onull
 
 instance IsNull Bool where
   isNull = id
+
+instance IsNull (Set.Set a) where
+  isNull = Set.null
+--
+-- | Nested is Null: is the container null, or are all of its items null?
+{-isNNull-}
+  {-:: (F.Foldable f)-}
+  {-=> f g -> Bool-}
+{-isNNull = F.all isNull-}
