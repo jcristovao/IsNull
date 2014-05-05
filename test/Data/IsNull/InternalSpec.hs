49f5512ecec220b16c09bc4aa307c6df6587ed6b
João Cristóvão <jmacristovao@gmail.com>
2014-05-05 15:55:14 +0100

    Updated tests to handle quickcheck 2.7

--- test/Data/IsNull/InternalSpec.hs
+++ test/Data/IsNull/InternalSpec.hs
@@ -47,44 +47,35 @@ spec = do
 -- Maybe ---------------------------------------------------------------------
 ------------------------------------------------------------------------------
   describe "IsNull" $ do
-    it "Equals null for lists" $ do
-      property $ \(x::[Int]) -> isNull x == L.null x
-      property $ \(x::[String]) -> isNull x == L.null x
-      property $ \(x::[Text]) -> isN x == L.null x
+    it "Equals null for lists (1)" $ property $ \(x::[Int]) -> isNull x == L.null x
+    it "Equals null for lists (2)" $ property $ \(x::[String]) -> isNull x == L.null x
+    it "Equals null for lists (3)" $ property $ \(x::[Text]) -> isN x == L.null x
 
-    it "Equals Data.Maybe.isJust for Maybe" $ do
-      property $ \(x :: Maybe Int) -> isNull x == isNothing x
-      property $ \(x :: Maybe String) -> isNull x == isNothing x
+    it "Equals Data.Maybe.isJust for Maybe (1)" $ property $ \(x :: Maybe Int) -> isNull x == isNothing x
+    it "Equals Data.Maybe.isJust for Maybe (2)" $ property $ \(x :: Maybe String) -> isNull x == isNothing x
 
-    it "Equals Data.Either.isRight (GHC 7.8) for Either" $ do
-      property $ \(x :: Either String String) -> isNull x == isLeft x
+    it "Equals Data.Either.isRight (GHC 7.8) for Either (1)" $ property $ \(x :: Either String String) -> isNull x == isLeft x
 
-    it "Equals Data.Set.null for Set" $ do
-      property $ \(x :: Set.Set String) -> isNull x == Set.null x
-      property $ \(x :: Set.Set Int) -> isNull x == Set.null x
+    it "Equals Data.Set.null for Set (1)" $ property $ \(x :: Set.Set String) -> isNull x == Set.null x
+    it "Equals Data.Set.null for Set (2)" $ property $ \(x :: Set.Set Int) -> isNull x == Set.null x
 
-    it "Equals Data.Map.null for Map" $ do
-      property $ \(x :: Map.Map Int String) -> isNull x == Map.null x
-      property $ \(x :: Map.Map String String) -> isNull x == Map.null x
-      property $ \(x :: Map.Map Int Int) -> isNull x == Map.null x
+    it "Equals Data.Map.null for Map (1)" $ property $ \(x :: Map.Map Int String) -> isNull x == Map.null x
+    it "Equals Data.Map.null for Map (2)" $ property $ \(x :: Map.Map String String) -> isNull x == Map.null x
+    it "Equals Data.Map.null for Map (3)" $ property $ \(x :: Map.Map Int Int) -> isNull x == Map.null x
 
-    it "Equals Data.IntMap.null for IntMap" $ do
-      property $ \(x :: IM.IntMap String) -> isNull x == IM.null x
-      property $ \(x :: IM.IntMap Int) -> isNull x == IM.null x
+    it "Equals Data.IntMap.null for IntMap (1)" $ property $ \(x :: IM.IntMap String) -> isNull x == IM.null x
+    it "Equals Data.IntMap.null for IntMap (2)" $ property $ \(x :: IM.IntMap Int) -> isNull x == IM.null x
 
-    it "Equals Data.Sequence.null for Sequence" $ do
-      property $ \(x  :: Seq.Seq String) -> isNull x == Seq.null x
-      property $ \(x  :: Seq.Seq Int   ) -> isNull x == Seq.null x
+    it "Equals Data.Sequence.null for Sequence (1)" $ property $ \(x  :: Seq.Seq String) -> isNull x == Seq.null x
+    it "Equals Data.Sequence.null for Sequence (2)" $ property $ \(x  :: Seq.Seq Int   ) -> isNull x == Seq.null x
 
-    it "Equals Data.HashSet.null for HashSet" $ do
-      property $ \(x :: HS.HashSet String)  -> isNull x == HS.null x
-      property $ \(x :: HS.HashSet Int   )  -> isNull x == HS.null x
+    it "Equals Data.HashSet.null for HashSet (1)" $ property $ \(x :: HS.HashSet String)  -> isNull x == HS.null x
+    it "Equals Data.HashSet.null for HashSet (2)" $ property $ \(x :: HS.HashSet Int   )  -> isNull x == HS.null x
 
-    it "Equals Data.HashMap.null for HashMap" $ do
-      property $ \(x :: HML.HashMap String String) -> isNull x == HML.null x
-      property $ \(x :: HMS.HashMap String String) -> isNull x == HMS.null x
-      property $ \(x :: HML.HashMap String Int   ) -> isNull x == HML.null x
-      property $ \(x :: HML.HashMap Int    Int   ) -> isNull x == HML.null x
+    it "Equals Data.HashMap.null for HashMap (1)" $ property $ \(x :: HML.HashMap String String) -> isNull x == HML.null x
+    it "Equals Data.HashMap.null for HashMap (2)" $ property $ \(x :: HMS.HashMap String String) -> isNull x == HMS.null x
+    it "Equals Data.HashMap.null for HashMap (3)" $ property $ \(x :: HML.HashMap String Int   ) -> isNull x == HML.null x
+    it "Equals Data.HashMap.null for HashMap (4)" $ property $ \(x :: HML.HashMap Int    Int   ) -> isNull x == HML.null x
 
     it "Equals Data.Vector.null for Vector" $ do
       property $ \(x :: V.Vector String) -> isNull x == V.null x
